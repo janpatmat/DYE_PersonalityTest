3eb3df1413ad269b997e52e99ccadbfecba2e459
Patrick <patmatayabas@gmail.com>
2023-04-12 20:58:17 +0800

    fixed css issue and header placement

---
+++ components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <>
+      <div className={styles['DYE-header']}>
+        <h1>Kota Learning Center</h1>
+         {/* <h2 className={styles['DYE-header_details']}>details</h2> */}
+         </div>
+    </>
+  )
+}
+
+export default Header
---
+++ stories/Header.stories.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header/Header';
+
+export default{
+    title: "Header",
+    component: Header,
+}
+
+export const Main = {
+    args: {}
+}
