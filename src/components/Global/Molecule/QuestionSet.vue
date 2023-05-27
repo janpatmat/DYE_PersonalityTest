1cdd2d3f4111f583fbfb32b32d689beef911671f
Patrick <patmatayabas@gmail.com>
2023-05-27 18:51:24 +0800

    changed questionpage function

--- src/components/Global/Molecule/QuestionSet.vue
+++ src/components/Global/Molecule/QuestionSet.vue
@@ -1,59 +1,113 @@
 <template>
-    <div class="container">
-      <h3>{{ question }}</h3>
-      <div class="container__choices">
-        <Ncircle :index="1" :selected="selectedComponent === 1" @click="selectComponent(1)" v-bind:class="{ 'selected-component': selectedComponent === 1 }"/>
-        <VRcircle :index="2" :selected="selectedComponent === 2" @click="selectComponent(2)" v-bind:class="{ 'selected-component': selectedComponent === 2 }"/>
-        <Rcircle :index="3" :selected="selectedComponent === 3" @click="selectComponent(3)" v-bind:class="{ 'selected-component': selectedComponent === 3 }"/>
-        <Fcircle :index="4" :selected="selectedComponent === 4" @click="selectComponent(4)" v-bind:class="{ 'selected-component': selectedComponent === 4 }"/>       
-        <VFcircle :index="5" :selected="selectedComponent === 5" @click="selectComponent(5)" v-bind:class="{ 'selected-component': selectedComponent === 5 }" />
-        <Acircle :index="6" :selected="selectedComponent === 6" @click="selectComponent(6)" v-bind:class="{ 'selected-component': selectedComponent === 6 }" />
-        
-      </div>
-      <div class="container__indicator">
-        <h3>Never</h3>
-        <h3>Always</h3>
-      </div>
+  <div class="container">
+    <h3>{{ question }}</h3>
+    <div class="container__choices">
+      <Ncircle
+        :index="1"
+        :selected="selectedComponent === 1"
+        @click="selectComponent(1)"
+        v-bind:class="{ 'selected-component': selectedComponent === 1 }"
+      />
+      <VRcircle
+        :index="2"
+        :selected="selectedComponent === 2"
+        @click="selectComponent(2)"
+        v-bind:class="{ 'selected-component': selectedComponent === 2 }"
+      />
+      <Rcircle
+        :index="3"
+        :selected="selectedComponent === 3"
+        @click="selectComponent(3)"
+        v-bind:class="{ 'selected-component': selectedComponent === 3 }"
+      />
+      <Fcircle
+        :index="4"
+        :selected="selectedComponent === 4"
+        @click="selectComponent(4)"
+        v-bind:class="{ 'selected-component': selectedComponent === 4 }"
+      />
+      <VFcircle
+        :index="5"
+        :selected="selectedComponent === 5"
+        @click="selectComponent(5)"
+        v-bind:class="{ 'selected-component': selectedComponent === 5 }"
+      />
+      <Acircle
+        :index="6"
+        :selected="selectedComponent === 6"
+        @click="selectComponent(6)"
+        v-bind:class="{ 'selected-component': selectedComponent === 6 }"
+      />
     </div>
-    <hr />
+    <div class="container__indicator">
+      <h3>Never</h3>
+      <h3>Always</h3>
+    </div>
+  </div>
+  <hr />
+</template>
 
-  </template>
-  
-  <script>
-  import Ncircle from "../CircleChoice/circleNever.vue";
-  import Fcircle from "../CircleChoice/circleFrequently.vue";
-  import VFcircle from "../CircleChoice/circleVF.vue";
-  import VRcircle from "../CircleChoice/circleVR.vue";
-  import Rcircle from "../CircleChoice/circleRarely.vue";
-  import Acircle from "../CircleChoice/circleAlways.vue";
-  
-  export default {
-    components: {
-      Ncircle,
-      Fcircle,
-      Acircle,
-      Rcircle,
-      VFcircle,
-      VRcircle,
+<script>
+import Ncircle from "../CircleChoice/circleNever.vue";
+import Fcircle from "../CircleChoice/circleFrequently.vue";
+import VFcircle from "../CircleChoice/circleVF.vue";
+import VRcircle from "../CircleChoice/circleVR.vue";
+import Rcircle from "../CircleChoice/circleRarely.vue";
+import Acircle from "../CircleChoice/circleAlways.vue";
+
+export default {
+  components: {
+    Ncircle,
+    Fcircle,
+    Acircle,
+    Rcircle,
+    VFcircle,
+    VRcircle,
+  },
+  data() {
+    return {
+      selectedComponent: null,
+    };
+  },
+  props: {
+    question: {
+      type: String,
+      required: true,
     },
-    data() {
-      return {
-        selectedComponent: null,
-      };
+    questionId: {
+      type: Number,
+      required: true,
     },
-    props: {
-      question: {
-        type: String,
-        required: true,
-      },
+  },
+  methods: {
+    selectComponent(index) {
+      this.selectedComponent = index;
+      const selectedText = this.getSelectedComponentText(index);
+      this.$emit("circleSelected", { questionId: this.questionId, selectedText });
+      console.log(`Clicked index: ${index}`);
     },
-    methods: {
-      selectComponent(index) {
-        this.selectedComponent = index;
-        console.log(`clicked ${index}`);
-      },
+    getSelectedComponentText(index) {
+      // Return the text based on the index
+      switch (index) {
+        case 1:
+          return -3;
+        case 2:
+          return -2;
+        case 3:
+          return -1;
+        case 4:
+          return 1;
+        case 5:
+          return 2;
+        case 6:
+          return 3;
+        default:
+          return "";
+      }
+
     },
-  };
+  },
+};
   </script>
   
   <style>
--- src/components/Global/Pages/QuestionPage.vue
+++ src/components/Global/Pages/QuestionPage.vue
@@ -1,30 +1,60 @@
 <template>
-    <div>
-      <QuestionSetVue v-for="question in questions" :key="question.id" :question="question.question" />
+  <div>
+    <QuestionSetVue
+      v-for="question in questions"
+      :key="question.id"
+      :question="question.question"
+      :questionId="question.id"
+      :group="question.group"
+      @circleSelected="accumulateSelectedText"
+    />
+    <div v-for="(total, group) in totals" :key="group">
+      Group {{ group }} Total: {{ total }}
     </div>
-  </template>
-  
-  <script>
-  import QuestionSetVue from '../Molecule/QuestionSet.vue';
-  
-  export default {
-    data() {
-      return {
-        questions: [
-          { id: 1, question: 'My name is question number 1' },
-          { id: 2, question: 'My name is question number 2' }
-        ]
-      };
+    <button @click="calculateTotals">Calculate Totals</button>
+  </div>
+</template>
+
+<script>
+import QuestionSetVue from '../Molecule/QuestionSet.vue';
+
+export default {
+  data() {
+    return {
+      questions: [
+        { id: 1, question: 'Question 1 (Group A)', group: 'A' },
+        { id: 2, question: 'Question 2 (Group A)', group: 'A' },
+        { id: 3, question: 'Question 3 (Group B)', group: 'B' }
+      ],
+      answerCounts: {},
+      totals: {}
+    };
+  },
+  components: {
+    QuestionSetVue
+  },
+  methods: {
+    accumulateSelectedText({ questionId, selectedText, group }) {
+      if (!this.answerCounts.hasOwnProperty(group)) {
+        this.answerCounts[group] = {};
+      }
+      if (!this.answerCounts[group].hasOwnProperty(questionId)) {
+        this.answerCounts[group][questionId] = 0;
+      }
+      this.answerCounts[group][questionId] += selectedText;
     },
-    components: {
-      QuestionSetVue
+    calculateTotals() {
+      this.totals = {};
+      for (const group in this.answerCounts) {
+        const groupAnswerCounts = Object.values(this.answerCounts[group]);
+        const groupTotal = groupAnswerCounts.reduce((sum, value) => sum + value, 0);
+        this.totals[group] = groupTotal;
+      }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-    /* Styles for the container */
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+/* Styles */
+</style>
